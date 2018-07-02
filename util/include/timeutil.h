f1d9274efa99acb9ccc436cf54665ffc4cc688e8
John Patton <jpatton@usgs.gov>
2018-07-02 12:28:05 -0600

    updated time util documentation

--- util/include/timeutil.h
+++ util/include/timeutil.h
@@ -14,49 +14,66 @@ namespace util {
 /**
  * \brief Convert time from epoch time to ISO8601
  *
- * Convert the given epoch time from decimal seconds to an
- * ISO8601 time string
- * \param epochtime - A double containing the epoch time
- * \return returns an ISO8601 formatted time std::string
+ * Convert the given epoch time (seconds from 1970) from decimal seconds to an
+ * ISO8601 time string in the format YYYY-MM-DDTHH:MM:SS.SSSZ, see
+ * https://en.wikipedia.org/wiki/ISO_8601 for a definition of ISO8601
+ *
+ * \param epochtime - A double containing the epoch time (seconds from 1970)
+ * \return returns a std::string containing the ISO8601 time string in the format
+ * YYYY-MM-DDTHH:MM:SS.SSSZ
  */
 std::string convertEpochTimeToISO8601(double epochtime);
 
 /**
  * \brief Convert time from epoch time to ISO8601
  *
- * Convert the given epoch time from decimal seconds to an
+ * Convert the given epoch time (seconds from 1970) from decimal seconds to an
  * ISO8601 time string
- * \param now - A time_t containing the epoch time
- * \param decimalseconds - A an optional double containing the decimal seconds
- * \return returns an ISO8601 formatted time std::string
+ * \param epochtime - A time_t containing the epoch time (seconds from 1970)
+ * \param decimalseconds - A an optional double containing the decimal seconds,
+ * default is 0
+ * \return returns a std::string containing the ISO8601 time string in the format
+ * YYYY-MM-DDTHH:MM:SS.SSSZ
  */
-std::string convertEpochTimeToISO8601(time_t now, double decimalseconds = 0);
+std::string convertEpochTimeToISO8601(std::time_t epochtime,
+										double decimalseconds = 0);
 
 /**
- * \brief Convert time from date time to ISO8601
+ * \brief Convert time from date time format to ISO8601 format
  *
- * Convert the given date time string to an
- * ISO8601 time string
- * \param TimeString - A std::string containing the date time
- * \return returns an ISO8601 formatted time std::string
+ * Convert the given DateTime time string in the format YYYYMMDDHHMMSS.SSS to an
+ * ISO8601 time string in the format YYYY-MM-DDTHH:MM:SS.SSSZ
+ *
+ * \param TimeString - A std::string containing the date time in the format
+ * YYYYMMDDHHMMSS.SS
+ * \return returns a std::string containing the ISO8601 time string in the format
+ * YYYY-MM-DDTHH:MM:SS.SSSZ
  */
 std::string convertDateTimeToISO8601(const std::string &TimeString);
 
 /**
  * \brief Convert time from date time to epoch time
  *
- * Convert the given datetime string to an epoch time
- * \param TimeString - A std::string containing the date time
- * \return returns a double variable containing the epochtime
+ * Convert the given DateTime time string in the format YYYYMMDDHHMMSS.SSS to an
+ * epoch time (seconds from 1970)
+ *
+ * \param TimeString - A std::string containing the date time in the format
+ * YYYYMMDDHHMMSS.SSS
+ * \return returns a double variable containing the epoch time (seconds from
+ * 1970)
  */
 double convertDateTimeToEpochTime(const std::string &TimeString);
 
 /**
  * \brief Convert time from ISO8601 time to epoch time
  *
- * Convert the given ISO8601 string to an epoch time
- * \param TimeString - A std::string containing the ISO8601 time
- * \return returns a double variable containing the epochtime
+ * Convert the given ISO8601 string in the format YYYY-MM-DDTHH:MM:SS.SSSZ to an
+ * epoch time
+ *
+ * \param TimeString - A std::string containing the ISO8601 time in the format
+ * YYYY-MM-DDTHH:MM:SS.SSSZ
+ * \return returns a double variable containing the epoch time (seconds from
+ * 1970)
  */
 double convertISO8601ToEpochTime(const std::string &TimeString);
 }  // namespace util
--- util/src/timeutil.cpp
+++ util/src/timeutil.cpp
@@ -23,6 +23,7 @@ namespace util {
  */
 char envTZ[MAXENV];
 
+// ----------------------------------------------------convertEpochTimeToISO8601
 std::string convertEpochTimeToISO8601(double epochtime) {
 	double integerpart;
 	double fractionpart;
@@ -34,18 +35,19 @@ std::string convertEpochTimeToISO8601(double epochtime) {
 	return (convertEpochTimeToISO8601(time, fractionpart));
 }
 
-std::string convertEpochTimeToISO8601(time_t time, double decimalseconds) {
+// ----------------------------------------------------convertEpochTimeToISO8601
+std::string convertEpochTimeToISO8601(time_t epochtime, double decimalseconds) {
 	// build the time portion, all but the seconds which are
 	// separate since time_t can't do decimal seconds
 	char timebuf[sizeof "2011-10-08T07:07:"];
 
 #ifdef _WIN32
 	struct tm timestruct;
-	gmtime_s(&timestruct, &time);
+	gmtime_s(&timestruct, &epochtime);
 
 #else
 	struct tm timestruct;
-	gmtime_r(&time, &timestruct);
+	gmtime_r(&epochtime, &timestruct);
 #endif
 
 	strftime(timebuf, sizeof timebuf, "%Y-%m-%dT%H:%M:", &timestruct);
@@ -66,12 +68,14 @@ std::string convertEpochTimeToISO8601(time_t time, double decimalseconds) {
 	return (timestring + secondsstring + "Z");
 }
 
+// ----------------------------------------------------convertDateTimeToISO8601
 std::string convertDateTimeToISO8601(const std::string &TimeString) {
 	double epoch_time = convertDateTimeToEpochTime(TimeString);
 
 	return (convertEpochTimeToISO8601(epoch_time));
 }
 
+// ----------------------------------------------------convertISO8601ToEpochTime
 double convertISO8601ToEpochTime(const std::string &TimeString) {
 	// make sure we got something
 	if (TimeString.length() == 0) {
@@ -197,6 +201,7 @@ double convertISO8601ToEpochTime(const std::string &TimeString) {
 	return (-1.0);
 }
 
+// --------------------------------------------------convertDateTimeToEpochTime
 double convertDateTimeToEpochTime(const std::string &TimeString) {
 	// make sure we got something
 	if (TimeString.length() == 0) {
