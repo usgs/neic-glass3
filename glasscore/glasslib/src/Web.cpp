53e50fa141335be4b88b83cc8bf14c5ef508a1c3
John Patton <jpatton@usgs.gov>
2020-03-27 12:25:07 -0600

    reduced extra logging

--- glasscore/glasslib/src/Web.cpp
+++ glasscore/glasslib/src/Web.cpp
@@ -1859,7 +1859,7 @@ void CWeb::removeSite(std::shared_ptr<CSite> site) {
 		return;
 	}
 
-	// first try to remove the site from the site list this is so 
+	// first try to remove the site from the site list this is so
 	// we don't pick it up again below
 	removeSiteFromSiteList(site);
 
@@ -1976,8 +1976,8 @@ void CWeb::removeSite(std::shared_ptr<CSite> site) {
 				if (node->getSite(newSite->getSCNL()) != NULL) {
 					continue;
 				}
-				
-				// we don't want an unused site, this should already be covered by the 
+
+				// we don't want an unused site, this should already be covered by the
 				// removeSiteFromSiteList call, but better safe than sorry
 				if (newSite->getIsUsed() == false) {
 					continue;
