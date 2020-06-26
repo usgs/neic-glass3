842197d4df47fef700ee0d923068fd9d3250320a
John Patton <jpatton@usgs.gov>
2020-06-26 08:16:57 -0600

    upped version

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,4 +1,4 @@
 # version.cmake - a CMake script that defines the overall project version
 set (PROJECT_VERSION_MAJOR 1)
 set (PROJECT_VERSION_MINOR 4)
-set (PROJECT_VERSION_PATCH 1)
+set (PROJECT_VERSION_PATCH 2)
